f0d09bde30fae1486d08c1ceeada3230305607aa
malechaS <29711868+malechaS@users.noreply.github.com>
2023-08-22 15:46:47 +0200

    adding empty components and home screen update FIXED

--- app/index.js
+++ app/index.js
@@ -11,25 +11,25 @@ const Home = () => {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen
-            options={{
-                headerStyle: { backgroundColor: COLORS.lightWhite },
-                headerShadowVisible: false,
-                headerLeft: () => (
-                    <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
-                ),
-                headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={icons.profile} dimension='100%' />
-                ),
-                headerTitle: ''
-            }}
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension='100%' />
+                    ),
+                    headerTitle: ''
+                }}
             />
 
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View
-                style={{
-                    flex: 1,
-                    padding: SIZES.medium
-                }}
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
                 >
                     <Welcome
 
